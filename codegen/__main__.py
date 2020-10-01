9bd94e827a7829d392aac2adf4a2e24f6339e67f
Johannes Bergmann <j-bergmann@outlook.com>
2020-10-01 17:02:01 +0200

    Auto-generating copyright header for yogi-python

--- codegen/__main__.py
+++ codegen/__main__.py
@@ -5,6 +5,7 @@ import pathlib
 import gen_yogi_core
 import gen_yogi_core_mock
 import gen_yogi_cpp
+import gen_yogi_python
 
 # Parse the core api YAML file
 with open(pathlib.Path(__file__).with_name('core_api.yaml')) as f:
@@ -14,3 +15,4 @@ with open(pathlib.Path(__file__).with_name('core_api.yaml')) as f:
 gen_yogi_core.generate(core_api)
 gen_yogi_core_mock.generate(core_api)
 gen_yogi_cpp.generate(core_api)
+gen_yogi_python.generate(core_api)
