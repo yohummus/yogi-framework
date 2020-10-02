ac4fbbdec12fa538b01b4a99fd0a82b020f2a82c
Johannes Bergmann <j-bergmann@outlook.com>
2020-10-02 04:41:45 +0200

    Simplified codegen main script

--- codegen/__main__.py
+++ codegen/__main__.py
@@ -9,17 +9,17 @@ import yaml
 import munch
 import pathlib
 import sys
+import importlib
 
-import gen_yogi_core
-import gen_yogi_core_mock
-import gen_yogi_cpp
-import gen_yogi_python
+CODEGEN_DIR = pathlib.Path(__file__).parent
 
 # Parse the core api YAML file
-with open(pathlib.Path(__file__).with_name('core_api.yaml')) as f:
+with open(CODEGEN_DIR / 'core_api.yaml') as f:
     core_api = munch.Munch().fromYAML(f)
 
 # Generate the code for the various sub-projects
-projects = sys.argv[1:] or ['yogi_core', 'yogi-core-mock', 'yogi-cpp', 'yogi-python']
-for project in projects:
-    sys.modules['gen_' + project.replace('-', '_')].generate(core_api)
+mods_from_cmdline = ['gen_' + x.replace('-', '_') for x in sys.argv[1:]]
+all_mods = [x.stem for x in CODEGEN_DIR.glob('gen_*.py')]
+
+for mod in mods_from_cmdline or all_mods:
+    importlib.import_module(mod).generate(core_api)
